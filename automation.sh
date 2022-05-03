e9e57f6395a20f814ac3f10a47bba264da3ef1f0
nishshah20 <shahn2004@gmail.com>
2022-05-04 02:09:46 +0530

    final changes

--- automation.sh
+++ automation.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-Name="Nishith Shah"
+#!/usr/bin/sudo bash
 DateTime=$(date '+%d%m%Y-%H%M%S')
 s3Bucket=upgrad-nishith
+Name="Nishith Shah"
 echo $Name
 sudo apt update -y
 sudo apt install apache2
@@ -25,3 +25,16 @@ tar -cf nishith-httpd-logs-$DateTime.tar access.log error.log
 cp nishith-httpd-logs-$DateTime.tar /tmp
 aws s3 cp /tmp/nishith-httpd-logs-$DateTime.tar s3://$s3Bucket/nishith-httpd-logs-$DateTime.tar
 echo "File uploaded to S3 bucket successful"
+file="/var/www/html/inventory.html"
+if [ ! -f "$file" ]
+then
+  echo "nishith-httpd-logs-$DateTime.tar" > /var/www/html/inventory.html
+else
+  echo "nishith-httpd-logs-$DateTime.tar" > /var/www/html/inventory.html
+fi
+echo "File created"
+cd /etc/cron.d
+sudo crontab -l > automation
+sudo echo "* * * * * root /root/Automation_Project/automation.sh >/dev/null 2>&1" > automation
+sudo crontab automation
+echo " Script completed"
